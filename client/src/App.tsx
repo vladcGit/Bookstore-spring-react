28155393862a4d94d5a19837874fef596c1d9861
Vlad <vlad.constantinescu65@yahoo.com>
2023-06-21 10:04:10 +0300

    added the LibraryService component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Carousel } from "./layouts/Homepage/Carousel";
 import { ExploreTopBooks } from "./layouts/Homepage/ExploreTopBooks";
 import { Hero } from "./layouts/Homepage/Heros";
+import LibraryServices from "./layouts/Homepage/LibraryServices";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <ExploreTopBooks />
       <Carousel />
       <Hero />
+      <LibraryServices />
     </div>
   );
 }
