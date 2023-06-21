23afe5e92482f023f43e521adbf726ad7be92a1d
Vlad <vlad.constantinescu65@yahoo.com>
2023-06-21 13:11:35 +0300

    Made the homepage to display books dinamically

--- client/src/layouts/Homepage/components/Carousel.tsx
+++ client/src/layouts/Homepage/components/Carousel.tsx
@@ -1,6 +1,54 @@
+import { useEffect, useState } from "react";
 import { ReturnBook } from "./ReturnBook";
+import BookModel from "../../../models/BookModel";
+import axios from "axios";
+import SpinnerLoading from "../../utils/SpinnerLoading";
 
 export const Carousel = () => {
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const baseUrl: string = "http://localhost:8080/api/books";
+      const url: string = `${baseUrl}?page=0&size=9`;
+
+      const res = await axios.get(url);
+      if (res.status !== 200) {
+        throw new Error("Something went wrong");
+      }
+
+      const loadedBooks: BookModel[] = [];
+      for (const book of res.data._embedded.books) {
+        const { _links, ...bookData } = book;
+        loadedBooks.push(bookData);
+      }
+
+      setBooks(loadedBooks);
+      setIsLoading(false);
+    };
+
+    try {
+      fetchBooks();
+    } catch (e: any) {
+      setIsLoading(false);
+      setHttpError(e.message);
+    }
+  }, []);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container mt-5" style={{ height: 550 }}>
       <div className="homepage-carousel-title">
@@ -16,23 +64,23 @@ export const Carousel = () => {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <div className="row d-flex justify-content-center align-items-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(0, 3).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(3, 6).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(6, 9).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
           <button
@@ -65,7 +113,7 @@ export const Carousel = () => {
       {/* Mobile */}
       <div className="d-lg-none mt-3">
         <div className="row d-flex justify-content-center align-items-center">
-          <ReturnBook />
+          <ReturnBook book={books[7]} key={books[7].id} />
         </div>
       </div>
       <div className="homepage-carousel-title mt-3">
--- client/src/layouts/Homepage/components/ReturnBook.tsx
+++ client/src/layouts/Homepage/components/ReturnBook.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import book_1000 from "../../../Images/BooksImages/book-luv2code-1000.png";
+import BookModel from "../../../models/BookModel";
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{ book: BookModel }> = ({ book }) => {
   return (
     <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
       <div className="text-center">
-        <img src={book_1000} width="151" height="233" alt="book" />
-        <h6 className="mt-2">Book</h6>
-        <p>Author</p>
+        <img src={book.img || book_1000} width="151" height="233" alt="book" />
+        <h6 className="mt-2">{book.title}</h6>
+        <p>{book.author}</p>
         <a className="btn main-color text-white" href="#">
           Reserve
         </a>
