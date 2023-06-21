63944b765add0933e658d7f916109b3ff26898f6
Vlad <vlad.constantinescu65@yahoo.com>
2023-06-21 13:10:39 +0300

    Made The rest repository readonly

---
+++ server/src/main/java/com/vlad/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,33 @@
+package com.vlad.springbootlibrary.config;
+
+import com.vlad.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    private String theAllowedOrigins = "http://localhost:5173";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.PUT, HttpMethod.DELETE};
+        config.exposeIdsFor(Book.class);
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+
+        cors.addMapping(config.getBasePath()+"/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] unsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) ->
+                        httpMethods.disable(unsupportedActions)
+                )
+                .withCollectionExposure((metadata, httpMethods) ->
+                        httpMethods.disable(unsupportedActions));
+    }
+}
