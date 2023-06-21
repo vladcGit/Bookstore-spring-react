492c1dff972d54503187c14fde557a9263df5ef1
Vlad <vlad.constantinescu65@yahoo.com>
2023-06-21 18:15:08 +0300

    Added pagination and search

---
+++ client/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -0,0 +1,223 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+import SpinnerLoading from "../utils/SpinnerLoading";
+import { SearchBook } from "./components/SearchBook";
+import axios from "axios";
+import { Pagination } from "../utils/Pagination";
+
+export const SearchBooksPage = () => {
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(5);
+  const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [search, setSearch] = useState("");
+  const [searchUrl, setSearchUrl] = useState("");
+  const [category, setCategory] = useState("Book category");
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const baseUrl: string = "http://localhost:8080/api/books";
+
+      let url: string = `${baseUrl}?page=${
+        currentPage - 1
+      }&size=${booksPerPage}`;
+
+      if (searchUrl !== "") {
+        let searchWithPage = searchUrl.replace(
+          "<pageNumber>",
+          `${currentPage - 1}`
+        );
+        url = baseUrl + searchWithPage;
+      }
+
+      const response = await axios.get(url);
+
+      if (response.status !== 200) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = response.data._embedded.books;
+
+      setTotalAmountOfBooks(response.data.page.totalElements);
+      setTotalPages(response.data.page.totalPages);
+
+      const loadedBooks: BookModel[] = [];
+
+      for (const key in responseData) {
+        loadedBooks.push({
+          id: responseData[key].id,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          description: responseData[key].description,
+          copies: responseData[key].copies,
+          copiesAvailable: responseData[key].copiesAvailable,
+          category: responseData[key].category,
+          img: responseData[key].img,
+        });
+      }
+
+      setBooks(loadedBooks);
+      setIsLoading(false);
+    };
+    try {
+      fetchBooks();
+    } catch (error: any) {
+      setIsLoading(false);
+      setHttpError(error.message);
+    }
+
+    window.scrollTo(0, 0);
+  }, [currentPage, searchUrl]);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  const searchHandleChange = () => {
+    setCurrentPage(1);
+    if (search === "") {
+      setSearchUrl("");
+    } else
+      setSearchUrl(
+        `/search/findByTitleContaining?title=${search}&page=<pageNumber>&size=${booksPerPage}`
+      );
+
+    setCategory("Book category");
+  };
+
+  const categoryField = (value: string) => {
+    setCurrentPage(1);
+    if (["fe", "be", "data", "devops"].includes(value.toLowerCase())) {
+      setCategory(value);
+      setSearchUrl(
+        `/search/findByCategory?category=${value}&page=<pageNumber>&size=${booksPerPage}`
+      );
+    } else {
+      setCategory("all");
+      setSearchUrl(`?page=<pageNumber>&size=${booksPerPage}`);
+    }
+  };
+
+  const indexOfLastBook: number = currentPage * booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+  let lastItem =
+    booksPerPage * currentPage <= totalAmountOfBooks
+      ? booksPerPage * currentPage
+      : totalAmountOfBooks;
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <div className="row mt-5">
+            <div className="col-6">
+              <div className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-labelledby="Search"
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <button
+                  className="btn btn-outline-success"
+                  onClick={searchHandleChange}
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {category}
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  <li onClick={() => categoryField("All")}>
+                    <a className="dropdown-item" href="#">
+                      All
+                    </a>
+                  </li>
+                  <li onClick={() => categoryField("fe")}>
+                    <a className="dropdown-item" href="#">
+                      Front End
+                    </a>
+                  </li>
+                  <li onClick={() => categoryField("be")}>
+                    <a className="dropdown-item" href="#">
+                      Back End
+                    </a>
+                  </li>
+                  <li onClick={() => categoryField("data")}>
+                    <a className="dropdown-item" href="#">
+                      Data
+                    </a>
+                  </li>
+                  <li onClick={() => categoryField("devops")}>
+                    <a className="dropdown-item" href="#">
+                      DevOps
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {totalAmountOfBooks > 0 ? (
+            <>
+              <div className="mt-3">
+                <h5>Number of results: ({totalAmountOfBooks})</h5>
+              </div>
+              <p>
+                {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks}{" "}
+                items:
+              </p>
+              {books.map((book) => (
+                <SearchBook book={book} key={book.id} />
+              ))}
+            </>
+          ) : (
+            <div className="m-5">
+              <h3>Can't find what you are looking for?</h3>
+              <a
+                type="button"
+                href="#"
+                className="btn main-color btn-md px-4 me-md-2 fw-bold text-white"
+              >
+                Library services
+              </a>
+            </div>
+          )}
+          {totalPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              paginate={paginate}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- server/src/main/java/com/vlad/springbootlibrary/dao/BookRepository.java
+++ server/src/main/java/com/vlad/springbootlibrary/dao/BookRepository.java
@@ -1,7 +1,13 @@
 package com.vlad.springbootlibrary.dao;
 
 import com.vlad.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContaining(@RequestParam("title")String title, Pageable pageable);
+    Page<Book> findByCategory(@RequestParam("category")String category, Pageable pageable);
+
 }
