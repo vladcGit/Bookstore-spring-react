14308a7976fdf913df66bf00a989f73de3974a44
Vlad <vlad.constantinescu65@yahoo.com>
2023-06-24 18:29:09 +0300

    Added reviews functionality

--- server/src/main/java/com/vlad/springbootlibrary/config/MyDataRestConfig.java
+++ server/src/main/java/com/vlad/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.vlad.springbootlibrary.config;
 
 import com.vlad.springbootlibrary.entity.Book;
+import com.vlad.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -15,7 +16,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.PUT, HttpMethod.DELETE};
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
+
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         cors.addMapping(config.getBasePath()+"/**")
                 .allowedOrigins(theAllowedOrigins);
---
+++ server/src/main/java/com/vlad/springbootlibrary/dao/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.vlad.springbootlibrary.dao;
+
+
+import com.vlad.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findBookById(@RequestParam("book_id") Long bookId, Pageable pageable);
+}
