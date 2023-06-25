85b924213ed16144325e04bd36ea824beac32b64
Vlad <vlad.constantinescu65@yahoo.com>
2023-06-25 11:07:06 +0300

    Added authentication on server side

---
+++ server/src/main/java/com/vlad/springbootlibrary/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.vlad.springbootlibrary.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/books/secure/**").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2ResourceServer(auth -> auth.jwt(Customizer.withDefaults()))
+                .cors(Customizer.withDefaults())
+                .setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+        Okta.configureResourceServer401ResponseBody(http);
+        return http.build();
+
+    }
+}
--- server/src/main/java/com/vlad/springbootlibrary/controller/BookController.java
+++ server/src/main/java/com/vlad/springbootlibrary/controller/BookController.java
@@ -2,6 +2,7 @@ package com.vlad.springbootlibrary.controller;
 
 import com.vlad.springbootlibrary.entity.Book;
 import com.vlad.springbootlibrary.service.BookService;
+import com.vlad.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,20 +14,21 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId) {
-        String userEmail = "testuser@email.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) {
+        String userEmail = ExtractJWT.payloadJwtExtraction(token, "\"sub\"");
+        System.out.println(userEmail);
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
-        String userEmail = "testuser@email.com";
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJwtExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJwtExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 
